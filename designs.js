8106efaccbe4a24d042557499fab552237867c3e
bjams <54215195+bjams@users.noreply.github.com>
2019-09-15 21:30:44 -0400

    Added comments and fixed grid clearing bug

--- designs.js
+++ designs.js
@@ -1,15 +1,21 @@
+// grid generation based on input
 function makeGrid() {
 
+    // storing necessary values
     const pixelCanvas = document.getElementById('pixelCanvas');
     const gridHeight = document.getElementById('gridHeight').value;
     const gridWidth = document.getElementById('gridWidth').value;
 
-    pixelCanvas.html = '';
+    // clearing grid upon clicking submit
+    pixelCanvas.innerHTML = '';
   
+    // inserting rows and cells
     for (let y = 0; y < gridHeight; y++) {
       let row = pixelCanvas.insertRow(y);
       for (let x = 0; x < gridWidth; x++) {
         let cell = row.insertCell(x);
+        // add an event when clicked 
+        // use selected color black is default
         cell.addEventListener('click', function(event) {
           event.target.style.backgroundColor = document.getElementById('pickColor').value;
         })
@@ -17,6 +23,7 @@ function makeGrid() {
     }
   }
 
+  // do a thing when when submitted
   document.getElementById('pickSize').addEventListener('submit', function(event) {
     event.preventDefault();
     makeGrid();
